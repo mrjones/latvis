688acbe8deae49e011ab02bad836b0eaa02822f6
Matt Jones <jonesmr+github@gmail.com>
2011-05-08 20:03:58 +0000

    more dead code

--- latvis.go
+++ latvis.go
@@ -1,31 +1,9 @@
 package main
 
 import (
-	"github.com/mrjones/latvis/latitude"
 	"github.com/mrjones/latvis/server"
-
-	"flag"
-	"fmt"
-	"log"
 )
 
-func GetApiHistorySource() *latitude.AuthorizedConnection {
-	connection := latitude.NewConnection()
-	tokenStore := latitude.NewTokenStorage("tokens.txt")
-
-  tokenSource := latitude.NewCachingTokenSource(connection, tokenStore);
-
-	fmt.Println("User to generate map for:")
-	var user string
-	fmt.Scanln(&user)
-
-	accessToken, err := tokenSource.GetToken(user);
-	if err != nil{ log.Fatal(err) }
-	return connection.Authorize(accessToken);
-}
-
 func main() {
-	flag.Parse()
-
   server.Serve()     
 }
