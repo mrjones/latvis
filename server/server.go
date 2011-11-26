c8a52e4ef2762cf5c2ede7ae5bdf6c160f22dbfb
Matt Jones <jonesmr+github@gmail.com>
2011-11-26 16:49:40 +0000

    fixed url escaping issue

--- server/server.go
+++ server/server.go
@@ -177,7 +177,8 @@ func propogateParameter(base string, params *url.Values, key string) string {
 		if len(base) > 0 {
 			base = base + "&"
 		}
-		base = base + key + "=" + params.Get(key)
+		// TODO(mrjones): sigh use the right library
+		base = base + key + "=" + url.QueryEscape(params.Get(key))
 	}
 	return base
 }
