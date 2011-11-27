b5d49dc1f7f5dd3b5b0fc5e33d6bd4e0f3bd17c4
Matt Jones <jonesmr+github@gmail.com>
2011-11-27 14:30:27 -0500

    more blobs test

--- server/blobs_test.go
+++ server/blobs_test.go
@@ -4,15 +4,43 @@ import (
 	"github.com/mrjones/gt"
 
 	"testing"
+	"url"
 )
 
-func TestHandleString(t *testing.T) {
-	h := &Handle{
+func simpleHandle() *Handle {
+	return &Handle{
 		timestamp: 0,
 		n1: 1,
 		n2: 2,
 		n3: 3,
 	}
 
+}
+
+func TestHandleDebugString(t *testing.T) {
+	h := simpleHandle()
+
 	gt.AssertEqualM(t, "0-123", h.String(), "Unexpected handle");
+}
+
+func TestHandleUrlString(t *testing.T) {
+	h := simpleHandle()
+
+	gt.AssertEqualM(t, 
+		"/page/0-1-2-3.xyz",
+		serializeHandleToUrl2(h, "xyz", "page"),
+		"Unexpected serialization")
+}
+
+func TestHandleToParams(t *testing.T) {
+	h := simpleHandle()
+
+	var p = make(url.Values)
+	
+	serializeHandleToParams(h, &p)
+	h2, err := parseHandleFromParams(&p)
+
+	gt.AssertNil(t, err)
+	gt.AssertEqualM(t, h, h2, "Should be equal")
+		
 }
