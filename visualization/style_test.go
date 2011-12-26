c2dca4b1a9ea00b38c7f8f94964301ae308143bc
Matt Jones <jonesmr+github@gmail.com>
2011-12-26 19:49:29 +0000

    first bwstyler test

--- visualization/style_test.go
+++ visualization/style_test.go
@@ -1,8 +1,38 @@
 package visualization
 
 import (
+	"github.com/mrjones/gt"
+
+	"image"
 	"testing"
 )
 
-func TestFoo(t *testing.T) {
+func TestBWStylerSimple(t *testing.T) {
+	g := NewGrid(2, 2)
+	g.Set(0, 0, 1)
+
+	styler := &BWStyler{}
+
+	img, err := styler.Style(g, 2, 2)
+	gt.AssertNil(t, err)
+	assertBlack(t, img.At(0, 0))
+	assertWhite(t, img.At(1, 0))
+	assertWhite(t, img.At(0, 1))
+	assertWhite(t, img.At(1, 1))
+}
+
+func assertBlack(t *testing.T, c image.Color) {
+	r, g, b, a := c.RGBA()
+	gt.AssertEqualM(t, uint32(0), r, "Red should be 0 for black")
+	gt.AssertEqualM(t, uint32(0), g, "Blue should be 0 for black")
+	gt.AssertEqualM(t, uint32(0), b, "Green should be 0 for black")
+	gt.AssertEqualM(t, uint32(0xFFFF), a, "Alpha should be max-uint32")
+}
+
+func assertWhite(t *testing.T, c image.Color) {
+	r, g, b, a := c.RGBA()
+	gt.AssertEqualM(t, uint32(0xFFFF), r, "Red should be max-uint32 for white")
+	gt.AssertEqualM(t, uint32(0xFFFF), g, "Blue should be max-uint32 for white")
+	gt.AssertEqualM(t, uint32(0xFFFF), b, "Green should be max-uint32 for white")
+	gt.AssertEqualM(t, uint32(0xFFFF), a, "Alpha should be max-uint32")
 }
