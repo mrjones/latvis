eb91da5866739e188afcb6287bbb1e52d713d6c1
Matt Jones <jonesmr+github@gmail.com>
2011-12-27 18:17:14 +0000

    cleaned up test comments

--- visualization/style_test.go
+++ visualization/style_test.go
@@ -10,23 +10,13 @@ import (
 	"testing"
 )
 
-//
-// WxH image (0,0) is in the top left:
-// (i.e. increasing Y-coordinates move downwards)
-// (0,0) ----- (W,0)
-//   |           |
-// (0,H) ----- (W,H)
-//
-// But in Lat/Lng, increasing Latitudes move upwards.
 func TestBWStyler2By2(t *testing.T) {
 	bounds, err := location.NewBoundingBox(
 		location.Coordinate{Lat: 0, Lng: 0},
 		location.Coordinate{Lat: 2, Lng: 2})
-
 	gt.AssertNil(t, err)
 
 	h := make(location.History, 0)
-
 	h = append(h, &location.Coordinate{Lat: .5, Lng: .5})
 
 	styler := &BWStyler{}
@@ -50,7 +40,6 @@ func TestBWStylerNotSquare(t *testing.T) {
 	bounds, err := location.NewBoundingBox(
 		location.Coordinate{Lat: 0, Lng: 0},
 		location.Coordinate{Lat: 3, Lng: 5})
-
 	gt.AssertNil(t, err)
 
 	h := make(location.History, 0)
@@ -74,6 +63,15 @@ func assertImage(t *testing.T, expected [][]string, actual image.Image) {
 	gt.AssertEqualM(t, len(expected), actual.Bounds().Dy(), "Unexpected image height")
 	gt.AssertEqualM(t, len(expected[0]), actual.Bounds().Dx(), "Unexpected image width")
 
+	// WxH image (0,0) is in the top left:
+	// (i.e. increasing Y-coordinates move downwards)
+	// (0,0) ----- (W,0)
+	//   |           |
+	// (0,H) ----- (W,H)
+	//
+	// This works, because the ASCII arrays in the asserts are laid out similarly.
+	// However, it means that printed coordinates might be y-inverted from
+  // intuition.
 	for x := 0 ; x < len(expected[0]) ; x++ {
 		for y := 0 ; y < len(expected) ; y++ {
 			expPix := expected[y][x]
